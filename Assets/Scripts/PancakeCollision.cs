c582316106e3a6f73f6ae23c3d0b1edc476eeae3
HyperDecahedron <108353203+HyperDecahedron@users.noreply.github.com>
2025-11-27 11:36:43 +0100

    added asset of a kitchen

---
+++ Assets/Scripts/PancakeCollision.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PancakeCollision : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Wall"))
+        {
+            if (rb != null)
+            {
+                rb.isKinematic = true;
+                rb.useGravity = false;
+            }
+
+            Destroy(gameObject, 60f); // destroy after 60 seconds
+        }
+    }
+}
+
