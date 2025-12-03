b0b3e79b690d9e87f157f16f099537ab655bfa1c
HyperDecahedron <108353203+HyperDecahedron@users.noreply.github.com>
2025-12-03 11:46:08 +0100

    added ps

--- Assets/Scripts/CookPancake.cs
+++ Assets/Scripts/CookPancake.cs
@@ -9,31 +9,48 @@ public class CookPancake : MonoBehaviour
 
     [Header("Cooking Times (seconds)")]
     public float timeToCook = 4f;   
-    public float timeToBurn = 8f;   
+    public float timeToBurn = 8f;
+
+    public PanManager pan; 
 
     private bool pancakeInside = false;
     private float cookTimer = 0f;
 
     private Renderer pancakeRenderer;
+    private bool triggered_burnt = false;
+    private int cooking_state = 0; 
 
     private void Update()
     {
         if (!pancakeInside || pancakeRenderer == null)
+        {
+            pan.Stop_PS_cooking();
             return;
+        }
 
         cookTimer += Time.deltaTime;
 
         // raw -> cooked -> burnt over time
         if (cookTimer >= timeToBurn)
         {
+            if (!triggered_burnt)
+            {
+                pan.Trigger_PS_overcooked();
+                triggered_burnt = true;
+                cooking_state = 2;
+            }
+            
             pancakeRenderer.material = pancakeBurnt;
         }
         else if (cookTimer >= timeToCook)
         {
+            cooking_state = 1;
             pancakeRenderer.material = pancakeCooked;
         }
         else
         {
+            pan.Trigger_PS_cooking();
+            cooking_state = 0;
             pancakeRenderer.material = pancakeRaw;
         }
     }
@@ -43,6 +60,7 @@ public class CookPancake : MonoBehaviour
         if (!other.CompareTag("Pancake"))
             return;
 
+        triggered_burnt = false;
         pancakeInside = true;
         cookTimer = 0f;
 
@@ -68,8 +86,12 @@ public class CookPancake : MonoBehaviour
         if (!other.CompareTag("Pancake"))
             return;
 
+        other.transform.gameObject.GetComponent<PancakeData>().state = cooking_state; // save if the pancake is burnt or not
+        cooking_state = 0;
         pancakeInside = false;
         cookTimer = 0f;
         pancakeRenderer = null;
+
     }
+
 }
